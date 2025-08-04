76d3038f637e2040cf05859d7ee749dc6e739a50
Maxime Briand <pro.maximebriand@gmail.com>
2025-08-04 22:01:27 +0200

    Compose Quadrant done

--- unit1_your_first_android_app/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ unit1_your_first_android_app/ComposeQuadrant/app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -14,11 +15,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,20 +48,55 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ComposeQuadrant(modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        Row(modifier = Modifier.fillMaxHeight(0.5f)) {
+        Row(
+            modifier = Modifier
+                .weight(0.5f)
+        ) {
             Quadrant(
                 title = stringResource(R.string.title_one),
                 content = stringResource(R.string.description_one),
-                color = colorResource(R.color.purple_dark),
+                modifier = Modifier
+                    .background(color = colorResource(R.color.purple_light))
+                    .weight(0.5f)
+                    .fillMaxSize()
+                    .padding(16.dp)
             )
             Quadrant(
-                title = stringResource(R.string.title_one),
-                content = stringResource(R.string.description_one),
-                color = colorResource(R.color.purple_dark),
+                title = stringResource(R.string.title_two),
+                content = stringResource(R.string.description_two),
+                modifier = Modifier
+                    .background(color = colorResource(R.color.purple_dark))
+                    .weight(0.5f)
+                    .fillMaxSize()
+                    .weight(0.5f)
+                    .padding(16.dp)
             )
         }
-        Row {
+        Row(
+            modifier = Modifier
+                .weight(0.5f)
+        ) {
+            Quadrant(
+                title = stringResource(R.string.title_three),
+                content = stringResource(R.string.description_three),
+                modifier = Modifier
+                    .background(colorResource(R.color.purple_very_dark))
+                    .weight(0.5f)
+                    .fillMaxSize()
+                    .weight(0.5f)
+                    .padding(16.dp)
 
+
+            )
+            Quadrant(
+                title = stringResource(R.string.title_four),
+                content = stringResource(R.string.description_four),
+                modifier = Modifier
+                    .background(color = colorResource(R.color.purple_very_light))
+                    .weight(0.5f)
+                    .fillMaxSize()
+                    .padding(16.dp)
+            )
         }
     }
 }
@@ -63,19 +105,21 @@ fun ComposeQuadrant(modifier: Modifier = Modifier) {
 fun Quadrant(
     title: String,
     content: String,
-    color: androidx.compose.ui.graphics.Color,
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = Modifier
-            .background(color = color)
-            .fillMaxWidth(0.5f)
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text(text = title)
-        Text(text = content)
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Text(text = content, textAlign = TextAlign.Justify)
+
     }
-
-
 }
 
 @Preview(showBackground = true)
