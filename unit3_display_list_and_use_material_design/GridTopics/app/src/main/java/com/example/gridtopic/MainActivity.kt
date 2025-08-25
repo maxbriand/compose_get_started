5346749424e9148c15a5ce9dc318ea54a87b29af
Maxime Briand <pro.maximebriand@gmail.com>
2025-08-25 11:54:58 +0200

    Unit 3: GridTopics done

--- unit3_display_list_and_use_material_design/GridTopics/app/src/main/java/com/example/gridtopic/MainActivity.kt
+++ unit3_display_list_and_use_material_design/GridTopics/app/src/main/java/com/example/gridtopic/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.gridtopic
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,17 +18,17 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gridtopic.ui.theme.GridTopicTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +49,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GripTopicLayout(modifier: Modifier = Modifier) {
     LazyVerticalGrid(
-        modifier = modifier, columns = GridCells.Adaptive(minSize = 200.dp)
+        columns = GridCells.Adaptive(minSize = 140.dp),
+        contentPadding = PaddingValues(all = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier,
     ) {
         items(DataSource.topics) { topic ->
             TopicCard(topic)
@@ -60,7 +65,6 @@ fun GripTopicLayout(modifier: Modifier = Modifier) {
 fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
     Card(modifier = Modifier.height(68.dp)) {
         Row(modifier = Modifier.height(68.dp)) {
-
             Image(
                 painter = painterResource(topic.image),
                 contentDescription = null,
@@ -68,13 +72,12 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
                     .height(68.dp)
                     .width(68.dp)
             )
-
             Column() {
                 Spacer(modifier = Modifier.height(16.dp))
                 Text(
                     text = stringResource(id = topic.name),
+                    style = MaterialTheme.typography.bodyMedium,
                     modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                    fontStyle = 
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 Row {
@@ -85,7 +88,10 @@ fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
                     )
                     Text(
                         text = "${topic.articleNumber}",
-                        modifier = Modifier.padding(start = 8.dp)
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                            .align(Alignment.CenterVertically),
                     )
                 }
             }
